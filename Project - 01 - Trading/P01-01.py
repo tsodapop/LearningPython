4fd1bfba0396992837405005d54d2fdc6ccd4a42
tsodapop <jonathantso93@gmail.com>
2018-06-18 23:49:06 -0500

    Learning how to have several lines on the same graph as well as including a second graph

--- Project - 01 - Trading/P01-01.py
+++ Project - 01 - Trading/P01-01.py
@@ -14,17 +14,17 @@ df.reset_index(inplace = True) #Creates a new index and pushes Date & Symbol to 
 df.set_index('Date',inplace = True) #Sets the index to be Date
 df = df.drop('Symbol',axis = 1) #remove the Symbol column
 
-print(df.tail(7)) #Gives the last 7 days from the date range specified
+# print(df.tail(7)) #Gives the last 7 days from the date range specified
 
-df['High'].plot()
-plt.legend()
+df['100MovingAvg'] = df['Close'].rolling(window=10, min_periods=0).mean()
+print(df.head())
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Close'])
+ax1.plot(df.index, df['100MovingAvg'])
+ax2.bar(df.index, df['Volume'])
+
 plt.show()
 
-'''------------------------------------------------------------------------------------------------------'''
-'''
-Things Learned:
-6/14
-1. reset_index and set_index allow you to choose which column you want as the actual index
-2.inplace = True is to update the variable df in place, meaning we don't have to do df = ... again 
-3. tail() gives you the number of Dates you want to be shown
-'''
