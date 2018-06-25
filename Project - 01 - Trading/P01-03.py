215629e18757df98cd52cfa6d66dcad5455d5e63
tsodapop <jonathantso93@gmail.com>
2018-06-24 23:56:20 -0500

    Updating to pull into a css for when you want to use the ticker data

--- Project - 01 - Trading/P01-03.py
+++ Project - 01 - Trading/P01-03.py
@@ -1,9 +1,10 @@
 import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as data
 import pickle #serializes an object (in this case, the S&P 500)
 import requests
-
-
-print('hello')
 
 def save_sp500_tickers():
 	resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -16,10 +17,25 @@ def save_sp500_tickers():
 		tickers.append(ticker)
 
 	with open("sp500tickers.pickle","wb") as f:
-		pickle.dump(tickers, f)
+		pickle.dump(tickers, f) 
 	print(tickers)
 
-	# return tickers
+	return tickers
 
-save_sp500_tickers()
+# save_sp500_tickers()
+
+def get_data_from_yahoo(reload_sp500=False):
+	if reload_sp500:
+		tickers = save_sp500_tickers()
+	else:
+		with open("sp500tickers.pickle","rb") as f:
+			tickers = pickle.load(f)
+	if not os.path.exists('stock_dfs'):
+		os.makedirs('stock_dfs')
+
+	start = dt.datetime(2018,1,1)
+	end = dt.datetime(2018,12,31)
+
+	for ticker in tickers:
+		
 
