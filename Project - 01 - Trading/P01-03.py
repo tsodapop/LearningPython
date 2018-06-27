d73f90340102c0dce687dcd7d70ad7a2737ebc85
tsodapop <jonathantso93@gmail.com>
2018-06-26 23:40:21 -0500

    Pulling in data into a single file

--- Project - 01 - Trading/P01-03.py
+++ Project - 01 - Trading/P01-03.py
@@ -22,7 +22,7 @@ def save_sp500_tickers():
 
 	return tickers
 
-# save_sp500_tickers()
+save_sp500_tickers()
 
 def get_data_from_yahoo(reload_sp500=False):
 	if reload_sp500:
@@ -36,7 +36,7 @@ def get_data_from_yahoo(reload_sp500=False):
 	start = dt.datetime(2018,1,1)
 	end = dt.datetime(2018,12,31)
 
-	for ticker in tickers[:10]: #first 10 tickers of S&P500
+	for ticker in tickers[:50]: #first 10 tickers of S&P500
 		print(ticker)
 		if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
 			df = data.DataReader(ticker, 'morningstar', start, end)
@@ -45,3 +45,29 @@ def get_data_from_yahoo(reload_sp500=False):
 			print('Already have {}'.format(ticker))
 
 get_data_from_yahoo()
+
+# def compile_data():
+# 	with open("sp500tickers.pickle","rb") as f:
+# 		tickers = pickle.load(f)
+
+# 	main_df = pd.DataFrame()
+
+# 	for count, ticker in enumerate(tickers):
+# 		df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+# 		df.set_index('Date', inplace=True)
+
+# 		df.rename(columns = {'Adj Close': ticker}, inplace=True)
+# 		df.drop(['Symbol','Open','High','Low','Close','Volume'], 1, inplace=True)
+
+# 		if main_df.empty:
+# 			main_df = df
+# 		else:
+# 			main_df = main_df.join(df, how='outer')
+
+# 		if count % 10 == 0:
+# 			print(count)
+
+# 	print(main_df.head())
+# 	main_df.to_csv('sp500_joined_closes.csv')
+
+# compile_data()
