4f437d7818970e63ac5a9cbb200cccf604114cf7
tsodapop <jonathantso93@gmail.com>
2018-06-27 23:45:43 -0500

    Getting data up to 40 tickers

--- Project - 01 - Trading/P01-03.py
+++ Project - 01 - Trading/P01-03.py
@@ -36,7 +36,7 @@ def get_data_from_yahoo(reload_sp500=False):
 	start = dt.datetime(2018,1,1)
 	end = dt.datetime(2018,12,31)
 
-	for ticker in tickers[:50]: #first 10 tickers of S&P500
+	for ticker in tickers[:40]: #first 10 tickers of S&P500
 		print(ticker)
 		if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
 			df = data.DataReader(ticker, 'morningstar', start, end)
@@ -46,28 +46,28 @@ def get_data_from_yahoo(reload_sp500=False):
 
 get_data_from_yahoo()
 
-# def compile_data():
-# 	with open("sp500tickers.pickle","rb") as f:
-# 		tickers = pickle.load(f)
+def compile_data():
+	with open("sp500tickers.pickle","rb") as f:
+		tickers = pickle.load(f)
 
-# 	main_df = pd.DataFrame()
+	main_df = pd.DataFrame()
 
-# 	for count, ticker in enumerate(tickers):
-# 		df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
-# 		df.set_index('Date', inplace=True)
+	for count, ticker in enumerate(tickers[:40]):
+		df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+		df.set_index('Date', inplace=True)
 
-# 		df.rename(columns = {'Adj Close': ticker}, inplace=True)
-# 		df.drop(['Symbol','Open','High','Low','Close','Volume'], 1, inplace=True)
+		df.rename(columns = {'Close': ticker}, inplace=True)
+		df.drop(['Symbol','Open','High','Low','Volume'], 1, inplace=True)
 
-# 		if main_df.empty:
-# 			main_df = df
-# 		else:
-# 			main_df = main_df.join(df, how='outer')
+		if main_df.empty:
+			main_df = df
+		else:
+			main_df = main_df.join(df, how='outer')
 
-# 		if count % 10 == 0:
-# 			print(count)
+		if count % 10 == 0:
+			print(count)
 
-# 	print(main_df.head())
-# 	main_df.to_csv('sp500_joined_closes.csv')
+	print(main_df.head())
+	main_df.to_csv('sp500_joined_closes.csv')
 
-# compile_data()
+compile_data()
---
+++ Project - 01 - Trading/P01-04.py
@@ -0,0 +1,17 @@
+import numpy as numpy
+import pandas as pd
+import pickle
+
+def process_data_for_labels(ticker):
+	hm_days = 5
+	df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
+	tickers = df.columns.values.tolist()
+	df.fillna(0, inplace=True)
+
+	for i in range(1, hm_days+1):
+		df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
+
+	df.fillna(0, inplace=True)
+	return tickers, df
+
+process_data_for_labels('AMD')	
